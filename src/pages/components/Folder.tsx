0004a38c278dcc94a4af3b71b32e1e3b3467494a
9utty <leegh4250@gmail.com>
2023-05-05 20:39:21 +0900

    feat: LANGUAGE 폴더 생성

--- src/pages/components/Folder.tsx
+++ src/pages/components/Folder.tsx
@@ -19,24 +19,32 @@ const Folder = ({ FolderName, func }: Props) => {
       >
         <div
           style={{
-            width: "50px",
-            display: "inline-block",
-            verticalAlign: "middle",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
-          <ImageTag Url="https://user-images.githubusercontent.com/86397600/236442704-86adb1a0-63f3-460a-a138-fcf18906222b.png" />
+          <div
+            style={{
+              width: "50px",
+              display: "inline-block",
+              verticalAlign: "middle",
+            }}
+          >
+            <ImageTag Url="https://user-images.githubusercontent.com/86397600/236442704-86adb1a0-63f3-460a-a138-fcf18906222b.png" />
+          </div>
+          <span
+            style={{
+              color: "white",
+              paddingLeft: "8px",
+              fontFamily: "dunggeunmo-bold",
+              fontSize: "20px",
+              zIndex: 0,
+            }}
+          >
+            {FolderName}
+          </span>
         </div>
-        <span
-          style={{
-            color: "white",
-            paddingLeft: "8px",
-            fontFamily: "dunggeunmo-bold",
-            fontSize: "20px",
-            zIndex: 0,
-          }}
-        >
-          {FolderName}
-        </span>
       </Button>
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,21 @@ import React from "react";
 import AppLayout from "./components/AppLayout";
 import Recommendation from "./components/Recommendation";
 import RCButton from "./components/RCButton";
+import { Row, Col } from "antd";
+import LangCategory from "./categorys/lang/Lang";
 
 const Home = () => {
   return (
     <div>
       <AppLayout>
-        <Recommendation />
+        <Row>
+          <Col xs={6} sm={6} md={4}>
+            <Recommendation />
+          </Col>
+          <Col xs={6} sm={6} md={4}>
+            <LangCategory />
+          </Col>
+        </Row>
       </AppLayout>
     </div>
   );
