5f1098de678ca6f77d9001f66abc4168fa421d61
9utty <leegh4250@gmail.com>
2023-05-08 22:51:03 +0900

    feat: New Post Moim?

--- src/pages/categorys/project/Project.tsx
+++ src/pages/categorys/project/Project.tsx
@@ -2,19 +2,22 @@ import React from "react";
 import { Row, Col, Grid } from "antd";
 import CategoryFolder from "@/pages/components/CategoryFolder";
 import CategoryHeader from "@/pages/components/CategoryHeader";
+import Moim1 from "./Moim/Moim1";
 
 const { useBreakpoint } = Grid;
 
 const ProjectCategory = () => {
   const screens = useBreakpoint();
-  const JavaScriptComponents = [...JSComponents].reverse();
+  const MoimCom = [...MoimComponents].reverse();
 
   return (
     <CategoryHeader HeaderName="PROJECT.">
       <Row>
         <Col xs={12} sm={8} md={6}>
           <CategoryFolder FolderName="Moim.">
-            <div></div>
+            {MoimCom.map(({ Component }, index) => {
+              return <Component key={index} />;
+            })}
           </CategoryFolder>
         </Col>
         <Col xs={12} sm={8} md={6}>
@@ -50,72 +53,24 @@ interface Post {
   Tag: string;
 }
 
-const JSPost: Post[] = [
+const MoimPost: Post[] = [
   {
-    Header: "JavaScript의 변천사",
-    Date: "2023년 05월 05일",
-    Tag: "JavaScript",
-  },
-  {
-    Header: "함수형 프로그래밍, 순수함수",
-    Date: "2023년 05월 06일",
-    Tag: "JavaScript",
-  },
-  {
-    Header: "일급함수, add_maker",
-    Date: "2023년 05월 07일",
-    Tag: "JavaScript",
-  },
-  {
-    Header: "Promise 기본 개념부터~",
-    Date: "2023년 05월 07일",
-    Tag: "JavaScript",
+    Header: "Moim?",
+    Date: "2023년 04월 06일",
+    Tag: "Project",
   },
 ];
 
-const JSComponents: Componets[] = [
-  //   {
-  //     Component: () => {
-  //       return (
-  //         <JavaScript1
-  //           Header={JSPost[0].Header}
-  //           Date={JSPost[0].Date}
-  //           Tag={JSPost[0].Tag}
-  //         />
-  //       );
-  //     },
-  //   },
-  //   {
-  //     Component: () => {
-  //       return (
-  //         <JavaScript2
-  //           Header={JSPost[1].Header}
-  //           Date={JSPost[1].Date}
-  //           Tag={JSPost[1].Tag}
-  //         />
-  //       );
-  //     },
-  //   },
-  //   {
-  //     Component: () => {
-  //       return (
-  //         <JavaScript3
-  //           Header={JSPost[2].Header}
-  //           Date={JSPost[2].Date}
-  //           Tag={JSPost[2].Tag}
-  //         />
-  //       );
-  //     },
-  //   },
-  //   {
-  //     Component: () => {
-  //       return (
-  //         <JavaScript4
-  //           Header={JSPost[3].Header}
-  //           Date={JSPost[3].Date}
-  //           Tag={JSPost[3].Tag}
-  //         />
-  //       );
-  //     },
-  //   },
+const MoimComponents: Componets[] = [
+  {
+    Component: () => {
+      return (
+        <Moim1
+          Header={MoimPost[0].Header}
+          Date={MoimPost[0].Date}
+          Tag={MoimPost[0].Tag}
+        />
+      );
+    },
+  },
 ];
