392b23a8fb86cf711e1db14ca5b9ab71f500801d
9utty <leegh4250@gmail.com>
2023-05-09 21:41:34 +0900

    feat: 새로운 포스팅 내 블로그 소개하기

--- src/pages/categorys/project/Project.tsx
+++ src/pages/categorys/project/Project.tsx
@@ -3,13 +3,14 @@ import { Row, Col, Grid } from "antd";
 import CategoryFolder from "@/pages/components/CategoryFolder";
 import CategoryHeader from "@/pages/components/CategoryHeader";
 import Moim1 from "./Moim/Moim1";
+import Win95Blog1 from "./MyBlog/Win95Blog1";
 
 const { useBreakpoint } = Grid;
 
 const ProjectCategory = () => {
   const screens = useBreakpoint();
   const MoimCom = [...MoimComponents].reverse();
-
+  const MyBlogCom = [...MyBlogComponents].reverse();
   return (
     <CategoryHeader HeaderName="PROJECT.">
       <Row>
@@ -32,7 +33,9 @@ const ProjectCategory = () => {
         </Col>
         <Col xs={12} sm={8} md={6}>
           <CategoryFolder FolderName="Win95Blog.">
-            <div></div>
+            {MyBlogCom.map(({ Component }, index) => {
+              return <Component key={index} />;
+            })}
           </CategoryFolder>
         </Col>
         {/** Category 컴포넌트들 작성 후 나열 */}
@@ -69,6 +72,28 @@ const MoimComponents: Componets[] = [
           Header={MoimPost[0].Header}
           Date={MoimPost[0].Date}
           Tag={MoimPost[0].Tag}
+        />
+      );
+    },
+  },
+];
+
+const MyBlogPost: Post[] = [
+  {
+    Header: "구리의 블로그!!!!",
+    Date: "2023년 05월 09일",
+    Tag: "Blog",
+  },
+];
+
+const MyBlogComponents: Componets[] = [
+  {
+    Component: () => {
+      return (
+        <Win95Blog1
+          Header={MyBlogPost[0].Header}
+          Date={MyBlogPost[0].Date}
+          Tag={MyBlogPost[0].Tag}
         />
       );
     },
