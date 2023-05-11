b1a61cfe39cab56bb5a2a4048df2d2f539a339f4
9utty <leegh4250@gmail.com>
2023-05-11 23:04:08 +0900

    feat: New Post

--- src/pages/categorys/network/Network/Network1.tsx
+++ src/pages/categorys/network/Network/Network1.tsx
@@ -16,7 +16,20 @@ interface Post {
 const Network1 = ({ Header, Date, Tag }: Post) => {
   return (
     <PostTemplate Header={Header} Date={Date} Tag={Tag}>
-      <div></div>
+      <H2>{`Port Number의 이해`}</H2>
+      <TextTag>{`- Port Number`}</TextTag>
+      <TabTag>
+        {`* Process 식별자`}
+        <TabTag>
+          {`~ 개발자의 입장에서는 Process가 무엇인지 알 수 있는 식별자라고 알고 있자`}
+        </TabTag>
+      </TabTag>
+      <TabTag>{`* Service 식별자`}</TabTag>
+      <TabTag>{`* Interface 식별자`}</TabTag>
+      <TextTag>{`- Port Number의 크기는 16bit이다`}</TextTag>
+      <TabTag>{`* 0 ~ 65535 범위인데 0과 65535는 안쓴다`}</TabTag>
+      <TabTag>{`2^16 - 2 라고 볼 수 있다`}</TabTag>
+      <TextTag>{`Port Number는 소켓에 바인딩 된다`}</TextTag>
     </PostTemplate>
   );
 };
