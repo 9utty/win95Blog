8369a43cc4e6cbcf0b8b2e9e2230f1a02a81d5b4
9utty <leegh4250@gmail.com>
2023-05-13 21:01:09 +0900

    feat: 사이트맵 추가

--- next.config.js
+++ next.config.js
@@ -1,9 +1,20 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
   experimental: {
     appDir: true,
   },
 };
 
-module.exports = nextConfig;
+module.exports = {
+  experimental: {
+    appDir: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/sitemap.xml",
+        destination: "/sitemap.xml",
+      },
+    ];
+  },
+};
