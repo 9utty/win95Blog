f105fa0465438a162cbe76e8a36ab378ede49dfe
9utty <leegh4250@gmail.com>
2023-05-20 17:48:20 +0900

    feat: Hacker New 레이아웃 완성

--- src/pages/hackerNews/HackerNews.tsx
+++ src/pages/hackerNews/HackerNews.tsx
@@ -1,9 +1,76 @@
-import React from "react";
+import { Grid, Modal, Row } from "antd";
+import React, { useState } from "react";
+import {
+  Button,
+  Frame,
+  Toolbar,
+  Window,
+  WindowContent,
+  WindowHeader,
+  Counter,
+  ScrollView,
+} from "react95";
 
 const HackerNewsURL = "https://api.hnpwa.com/v0/news/1.json";
+const { useBreakpoint } = Grid;
 
 const HackerNews = () => {
-  return <div>HackerNews</div>;
+  const [isOpen, setIsOpen] = useState(false);
+  const screens = useBreakpoint();
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <div>
+      {isOpen && (
+        <Modal>
+          <Window
+            className="window"
+            style={{
+              position: "absolute",
+              top: screens.md ? "50%" : "0%",
+              left: screens.md ? "50%" : "0%",
+              width: screens.md ? "80vw" : "99vw",
+              height: "60vh",
+              transform: screens.md
+                ? "translate(-50%, -50%)"
+                : "translate(0%, 15%)",
+            }}
+          >
+            <WindowHeader
+              className="window-title"
+              style={{ justifyContent: "space-between", display: "flex" }}
+            >
+              <span style={{ fontFamily: "dunggeunmo-bold", fontSize: "22px" }}>
+                Hacker News
+              </span>
+              <Button style={{ marginTop: "3px" }} onClick={closeModal}>
+                <span
+                  style={{ fontFamily: "dunggeunmo-bold", fontSize: "20px" }}
+                >
+                  X
+                </span>
+              </Button>
+            </WindowHeader>
+            <WindowContent>
+              <Row>
+                <ScrollView
+                  shadow={false}
+                  style={{ width: "100%", height: "44vh" }}
+                ></ScrollView>
+              </Row>
+            </WindowContent>
+          </Window>
+        </Modal>
+      )}
+    </div>
+  );
 };
 
 export default HackerNews;
